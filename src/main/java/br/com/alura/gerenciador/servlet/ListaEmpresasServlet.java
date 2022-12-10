49e53d9b0b020bdd34d1e94ef710ee248e5d73b8
igorlops <claudioigorai@gmail.com>
2022-12-10 13:15:32 -0300

    implementando EL e JSTL

--- src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -1,9 +1,9 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ListaEmpresasServlet
  */
-@WebServlet("/ListaEmpresas")
+@WebServlet("/listaEmpresas")
 public class ListaEmpresasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -23,17 +23,13 @@ public class ListaEmpresasServlet extends HttpServlet {
 
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
-		PrintWriter out = response.getWriter();
 		
-		out.println("<html><body>");
-		out.println("<ul>");
 		
-		for (Empresa empresa : lista) {
-			out.println("<li> "+empresa.getNome()+ "</li>");
-		}
+		request.setAttribute("empresas", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("/ListagemEmpresas.jsp");
+		rd.forward(request,response);
 		
-		out.println("</ul>");
-		out.println("</body></html>");
+		
 		
 	}
 
---
+++ src/webapp/formNovaEmpresa.jsp
@@ -0,0 +1,18 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:url value="/novaEmpresa" var="linkServletNovaEmpresa"/>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<form action="${linkServletNovaEmpresa}" method="POST">
+		Nome: <input type="text" name="nome"/><br/>
+		Data Abertura: <input type="text" name="data"/>
+		<input type="submit"/>
+	</form>
+</body>
+</html>
